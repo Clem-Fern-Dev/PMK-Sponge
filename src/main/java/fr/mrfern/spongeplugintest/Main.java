15e05ae4d088758568512ecb8cbdff4767549346
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-03 15:12:20 +0100

    Fix plugin status

--- src/main/java/fr/mrfern/spongeplugintest/Main.java
+++ src/main/java/fr/mrfern/spongeplugintest/Main.java
@@ -1,9 +1,14 @@
 package fr.mrfern.spongeplugintest;
 
+import java.nio.file.Path;
+
 import org.slf4j.Logger;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GameLoadCompleteEvent;
+import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
-import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 import com.google.inject.Inject;
@@ -14,14 +19,53 @@ public class Main {
 	@Inject
 	private Logger logger;
 	
-	@Listener
-    public void onServerStart(GameStartedServerEvent event) {
-				
-    }
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	private Path defaultConfig;
 	
 	@Listener
-	public void onLoadPlugin(GamePreInitializationEvent event) {
+	public void onPreInit(GamePreInitializationEvent event) {
+		
 		logger.info("Lancement plugin");
+		
+		/*L’événement GamePreInitializationEvent est levé. Durant cet état, le plugin se prépare à l’initialisation. 
+		 * Les accès à l’instance du logger par défaut et aux informations concernant les localisations de fichiers de configurations préférées 
+		 * sont disponibles.
+		 */
+		
+		
+		
+		
 	}
-
+	
+	@Listener
+	public void onInit(GameInitializationEvent event) {
+		
+		logger.info("Plugin Init");
+		
+		/*L’événement GameInitializationEvent est levé. Durant cet état, le plugin devrait avoir finit tout ce qu’il avait à faire afin de fonctionner. 
+		 * Les gestionnaires d’événements sont traités à ce moment là.
+		 */
+		
+	}
+	
+	@Listener
+	public void onPostInit(GamePostInitializationEvent event) {
+		
+		logger.info("Plugin Post Init");
+		
+		/* L’événement GamePostInitializationEvent est levé. Par cet état, les communications inter-plugin devraient être prêtes à se produire. 
+		 * Les plugins fournissant une API devraient être prêts à accepter des requêtes de base.
+		 */
+		
+	}
+	
+	@Listener
+	public void onInitComplete(GameLoadCompleteEvent event) {
+		
+		logger.info("Plugin Load complete");
+		
+		//L’événement GameLoadCompleteEvent est levé. Par cet état, toutes les initialisations des plugins devraient être terminées.
+		
+	}
 }
