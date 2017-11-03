9252b9f34904807b9d61ad03a6892f4b4e57b706
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-03 12:09:50 +0100

    update

--- src/main/java/fr/mrfern/spongeplugintest/Main.java
+++ src/main/java/fr/mrfern/spongeplugintest/Main.java
@@ -1,17 +1,27 @@
 package fr.mrfern.spongeplugintest;
 
+import org.slf4j.Logger;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+
+import com.google.inject.Inject;
 
 @Plugin(id = "spongeplugintest", name = "spongeplugintest", version = "1.0")
 public class Main {
 	
+	@Inject
+	private Logger logger;
+	
 	@Listener
     public void onServerStart(GameStartedServerEvent event) {
-		
-		
-		
+				
     }
+	
+	@Listener
+	public void onLoadPlugin(GamePreInitializationEvent event) {
+		logger.info("Lancement plugin");
+	}
 
 }
