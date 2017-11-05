21658511c66f270269a389661fc18196f125ae3a
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-05 01:11:38 +0100

    Update ressource

--- src/main/java/fr/mrfern/spongeplugintest/config/Config.java
+++ src/main/java/fr/mrfern/spongeplugintest/config/Config.java
@@ -1,15 +1,6 @@
 package fr.mrfern.spongeplugintest.config;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.Charset;
-
-import com.google.gson.stream.JsonReader;
 
 public class Config implements IConfig{
 
