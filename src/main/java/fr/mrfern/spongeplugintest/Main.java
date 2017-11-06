0af491aef7a890d29532fb1a6a72e8d0d22d6be8
Enzo <Enzo@DESKTOP-EUC42O3>
2017-11-06 22:53:58 +0100

    fix import and build messageCommandResult

--- src/main/java/fr/mrfern/spongeplugintest/Main.java
+++ src/main/java/fr/mrfern/spongeplugintest/Main.java
@@ -15,7 +15,7 @@ import org.spongepowered.api.text.Text;
 
 import com.google.inject.Inject;
 
-import fr.mrfern.spongeplugintest.command.HelloWorldCommand;
+import fr.mrfern.spongeplugintest.command.*;
 import fr.mrfern.spongeplugintest.config.Config;
 import fr.mrfern.spongeplugintest.config.PlayerConfig;
 
@@ -111,7 +111,7 @@ public class Main {
 				.arguments(
 						GenericArguments.onlyOne(GenericArguments.player(Text.of("player"))),
 						GenericArguments.remainingJoinedStrings(Text.of("message")))
-				.executor(new fr.mrfern.spongeplugintest.command.MessageCommand())
+				.executor(new MessageCommand())
 				.build();
 		
 		Sponge.getCommandManager().register(this, myCommandSpec, "helloworld");
