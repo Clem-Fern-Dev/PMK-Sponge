35f64c52afbb2541698c76bce0d0d8a2740393a3
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-10 23:32:13 +0100

    Fix commandresult succes

--- src/main/java/fr/mrfern/spongeplugintest/chunk/commands/ChunkCommand.java
+++ src/main/java/fr/mrfern/spongeplugintest/chunk/commands/ChunkCommand.java
@@ -1,6 +1,5 @@
 package fr.mrfern.spongeplugintest.chunk.commands;
 
-import java.awt.Color;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -10,6 +9,7 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.util.Color;
 
 import fr.mrfern.spongeplugintest.config.ChunkConfig;
 import fr.mrfern.spongeplugintest.config.ChunkNode;
@@ -24,7 +24,23 @@ public class ChunkCommand implements CommandExecutor {
 					(int) player.getLocation().getX(),
 					(int) player.getLocation().getZ());
 		    
-		    player.sendMessage(Text.of(Color.BLUE + "[ ChunkInfo X:" + chunkNode.getLocationX() + " / Z:" + chunkNode.getLocationZ() + " ] "));
+		    if(chunkNode != null) {
+		    	
+		    	String claimORnot;
+			    
+			    if(chunkNode.getClaimedBy().equalsIgnoreCase("none")) {
+			    	claimORnot = " est disponible";
+			    }else {
+			    	claimORnot = "a été claim par " + Color.NAVY + chunkNode.getClaimedBy();
+			    }
+			    
+			    player.sendMessage(Text.of(Color.BLUE + "[ ChunkInfo X:" + 
+			    							Color.MAGENTA + chunkNode.getLocationX() + 
+			    							Color.BLUE + " / Z:" + 
+			    							Color.MAGENTA + chunkNode.getLocationZ() + Color.BLUE + " ] Ce chunk " + claimORnot + Color.BLUE + " ..."));
+			    return CommandResult.success();
+			    
+		    }
 		    
 		}
 		else if(src instanceof ConsoleSource) {
