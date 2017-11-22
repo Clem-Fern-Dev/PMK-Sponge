ba3fe545945c605131b76bebffed382019be942b
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-22 22:48:10 +0100

    Ajout package Player & PlayerListenerManager

---
+++ src/main/java/fr/mrfern/spongeplugintest/player/PlayerListenerManager.java
@@ -0,0 +1,21 @@
+package fr.mrfern.spongeplugintest.player;
+
+import org.spongepowered.api.Sponge;
+
+import fr.mrfern.spongeplugintest.Main;
+
+public class PlayerListenerManager {
+
+	private static PlayerListenerManager instance = new PlayerListenerManager();
+	private static Main mainManager;
+
+		public static PlayerListenerManager listener(Main main) {
+			mainManager = main;
+			return instance;
+		}
+		 
+		public void setup() {
+			Sponge.getEventManager().registerListeners(mainManager, new PlayerInstancieManager(mainManager.getLogger()));
+		}
+	
+}
