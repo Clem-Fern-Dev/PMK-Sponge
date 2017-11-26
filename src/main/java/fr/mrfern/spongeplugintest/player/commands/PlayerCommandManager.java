a1af4fe111755c71506e1c078326095f7359e058
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-26 01:28:14 +0100

    Ajout class CommandBanManager & TBan/UnBan class

--- src/main/java/fr/mrfern/spongeplugintest/player/commands/PlayerCommandManager.java
+++ src/main/java/fr/mrfern/spongeplugintest/player/commands/PlayerCommandManager.java
@@ -1,5 +1,35 @@
 package fr.mrfern.spongeplugintest.player.commands;
 
-public class PlayerCommandManager {
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
 
+import fr.mrfern.spongeplugintest.Main;
+
+public class PlayerCommandManager {
+	private static PlayerCommandManager instance = new PlayerCommandManager();
+	private static Main mainManager;
+
+	public static PlayerCommandManager commands(Main main) {
+		mainManager = main;
+		return instance;
+	}
+	
+	public void setupCommands() {
+		
+		CommandSpec commandTempBanSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .executor(new TBanCommand())
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandTempBanSpec, "tban");
+		
+		CommandSpec commandUnBanSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .executor(new UnBanCommand())
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandUnBanSpec, "unban");
+		
+	}
 }
