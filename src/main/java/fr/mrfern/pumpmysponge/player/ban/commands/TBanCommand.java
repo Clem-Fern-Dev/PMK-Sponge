78a4458730a6b9760e59299770ade02c357814cd
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-27 23:12:24 +0100

    save

--- src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/TBanCommand.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/TBanCommand.java
@@ -1,5 +1,10 @@
 package fr.mrfern.pumpmysponge.player.ban.commands;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -9,12 +14,15 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 
 import fr.mrfern.pumpmysponge.player.IPermissions;
 
 public class TBanCommand implements CommandExecutor,IPermissions {
 
+	private Object AcceptOrNo = true;
+	
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 		
@@ -24,25 +32,51 @@ public class TBanCommand implements CommandExecutor,IPermissions {
 		    
 			Player ply = (Player) src;
 			
-			if(checkPermissions(ply, "pumpmystaff.ban")) {
+			Player target;
+			String time;
+			HashMap<TimeEnum, String> hashTime = new HashMap<>();
+			
+			if(args.<Player>getOne("player").isPresent()) {
+				target = args.<Player>getOne("player").get();
+			}else {
 				
-				Player target;
+				Text textClaimed = Text.builder("Le joueur spécifié n'est pas valide ").color(TextColors.RED).build();
+		    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+	    		ply.sendMessage(textEnTete);
+			    return CommandResult.empty();
+			}
+			
+			if(args.<Player>getOne("time").isPresent()) {
 				
-				if(args.<Player>getOne("player").isPresent()) {
-					target = args.<Player>getOne("player").get();
-				}else {
-					
-					Text textClaimed = Text.builder("Le joueur spécifié n'est pas valide ").color(TextColors.RED).build();
-			    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
-		    		ply.sendMessage(textEnTete);
-				    return CommandResult.empty();
+				time = args.<String>getOne("time").get();
+				
+				ply.sendMessage(Text.of(time));
+				
+				String[] listTime = time.split("");
+				
+				for (String splitedTime : listTime) {
+					if(!splitedTime.endsWith("Y") & !splitedTime.endsWith("M") & !splitedTime.endsWith("D") & !splitedTime.endsWith("H") &!splitedTime.endsWith("M")) {
+						
+						Text textClaimed = Text.builder("Le temps spécifié n'est pas valide, faite /bhelp pour voir les commandes , le temps").color(TextColors.RED).build();
+				    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+			    		ply.sendMessage(textEnTete);
+					    return CommandResult.empty();
+						
+					}
 				}
 				
 				
+			}else{
 				
-				
-				
+				Text textClaimed = Text.builder("Le temps spécifié n'est pas valide, faite /bhelp pour voir les commandes").color(TextColors.RED).build();
+		    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+	    		ply.sendMessage(textEnTete);
+			    return CommandResult.empty();
 			}
+			
+			
+			
+						
 			
 			return CommandResult.empty();
 			
@@ -54,5 +88,8 @@ public class TBanCommand implements CommandExecutor,IPermissions {
 		return CommandResult.empty();
 		
 	}
-
+	
+	public HashMap<TimeEnum, String> SplitTimer(String timer) {
+		return null;		
+	}
 }
