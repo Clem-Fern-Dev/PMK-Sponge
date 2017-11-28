8edaacb29d8a8ca54f1857063540a990266dcaa4
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-28 20:25:55 +0100

    Ajout split() time argument

--- src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/TBanCommand.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/TBanCommand.java
@@ -1,10 +1,6 @@
 package fr.mrfern.pumpmysponge.player.ban.commands;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -14,25 +10,20 @@ import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 
 import fr.mrfern.pumpmysponge.player.IPermissions;
 
 public class TBanCommand implements CommandExecutor,IPermissions {
 
-	private Object AcceptOrNo = true;
-	
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-		
-		boolean all = false;
 		
 		if(src instanceof Player) {
 		    
 			Player ply = (Player) src;
-			
 			Player target;
+			
 			String time;
 			HashMap<TimeEnum, String> hashTime = new HashMap<>();
 			
@@ -51,20 +42,37 @@ public class TBanCommand implements CommandExecutor,IPermissions {
 				time = args.<String>getOne("time").get();
 				
 				ply.sendMessage(Text.of(time));
+				hashTime.clear();
 				
-				String[] listTime = time.split("");
+				String[] listTime = time.split(" ");
 				
 				for (String splitedTime : listTime) {
-					if(!splitedTime.endsWith("Y") & !splitedTime.endsWith("M") & !splitedTime.endsWith("D") & !splitedTime.endsWith("H") &!splitedTime.endsWith("M")) {
+					if(!splitedTime.endsWith("D") & !splitedTime.endsWith("H") &!splitedTime.endsWith("M")) {
 						
 						Text textClaimed = Text.builder("Le temps spécifié n'est pas valide, faite /bhelp pour voir les commandes , le temps").color(TextColors.RED).build();
 				    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
 			    		ply.sendMessage(textEnTete);
 					    return CommandResult.empty();
 						
+					}else {
+						ply.sendMessage(Text.of(splitedTime.charAt(splitedTime.length()-1)));
+						switch (splitedTime.charAt(splitedTime.length()-1)) {
+							
+							case 'D':
+									hashTime.putIfAbsent(TimeEnum.Day, splitedTime);
+								break;
+							case 'H':
+									hashTime.putIfAbsent(TimeEnum.Hour, splitedTime);
+								break;
+							case 'm':
+								hashTime.putIfAbsent(TimeEnum.Minute, splitedTime);
+								break;
+						}
 					}
 				}
 				
+				ply.sendMessage(Text.of(target.getName() + "  " + hashTime.get(TimeEnum.Day) + "  " + hashTime.get(TimeEnum.Hour) + "  " + hashTime.get(TimeEnum.Minute)));
+				return CommandResult.success();
 				
 			}else{
 				
@@ -73,12 +81,6 @@ public class TBanCommand implements CommandExecutor,IPermissions {
 	    		ply.sendMessage(textEnTete);
 			    return CommandResult.empty();
 			}
-			
-			
-			
-						
-			
-			return CommandResult.empty();
 			
 		}
 		else if(src instanceof ConsoleSource | src instanceof CommandBlockSource){
