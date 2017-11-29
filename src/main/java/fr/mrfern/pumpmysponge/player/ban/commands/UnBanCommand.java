a586240824e4852e8c2e9b05758c72b939e628f9
CrazyDoC-Dev <clement.coussieu@gmail.com>
2017-11-29 21:14:54 +0100

    Vérification si joueur valide dans UnBanCommand

--- src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/UnBanCommand.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/ban/commands/UnBanCommand.java
@@ -1,5 +1,7 @@
 package fr.mrfern.pumpmysponge.player.ban.commands;
 
+import java.util.HashMap;
+
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -8,19 +10,82 @@ import org.spongepowered.api.command.source.CommandBlockSource;
 import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import fr.mrfern.pumpmysponge.config.PlayerConfig;
+import fr.mrfern.pumpmysponge.config.PlayerNode;
 
 public class UnBanCommand implements CommandExecutor {
 
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 		
-		@SuppressWarnings("unused")
-		boolean all = false;
-		
-		if(src instanceof Player) {
+if(src instanceof Player) {
 		    
-			if (args.hasAny("all")) {
-			   // alors ban all
+			Player ply = (Player) src;
+			Player target;
+			
+			Integer day = 0, hour = 0,minute = 0;
+			String raison;
+			
+			HashMap<TimeEnum, Integer> hashTime = new HashMap<>();
+			
+			if(args.<Player>getOne("player").isPresent()) {
+				target = args.<Player>getOne("player").get();
+			}else {
+				 
+				Text textClaimed = Text.builder("Le joueur spécifié n'est pas valide ").color(TextColors.RED).build();
+		    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+	    		ply.sendMessage(textEnTete);
+			    return CommandResult.empty();
+			}	
+					
+			PlayerNode targetNode = PlayerConfig.getInstance().getPlayerConfigNode(target.getUniqueId());
+			
+			if(targetNode.getIsBanned()) {
+				
+				// joueur ban
+				targetNode.setPlayerIsBanned(false);	 
+					
+				// ajout des infos
+				//targetNode.setPlayerBanRaison(raison);	// raison du bannissement
+				
+				// ajout date du bannissement
+				
+				// temps de bannissement
+				targetNode.setPlayerBanTimeDay(hashTime.get(TimeEnum.Day));
+				targetNode.setPlayerBanTimeHour(hashTime.get(TimeEnum.Hour));
+				targetNode.setPlayerBanTimeMinut(hashTime.get(TimeEnum.Minute));
+										
+				// info du bannisseur
+				targetNode.setPlayerBanAuthorName(ply.getName());
+				targetNode.setPlayerBanAuthorUUID(ply.getUniqueId());
+				
+				targetNode.save();
+				
+			}else {
+				
+				Text textClaimed = Text.builder("Le joueur spécifié n'est pas banni ").color(TextColors.RED).build();
+		    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+	    		ply.sendMessage(textEnTete);
+			    return CommandResult.empty();
+			    
+			}
+					
+					
+						
+					
+		
+				}else {
+					
+					Text textClaimed = Text.builder("Il faut une raison valide ( non vide, plus de 8 caractères )").color(TextColors.RED).build();
+			    	Text textEnTete = Text.builder("[ PumpMyStaff ] ").color(TextColors.GOLD).append(textClaimed).build();
+		    		ply.sendMessage(textEnTete);
+				    return CommandResult.empty();
+				    
+				}			
+				
 			}
 			
 		}
