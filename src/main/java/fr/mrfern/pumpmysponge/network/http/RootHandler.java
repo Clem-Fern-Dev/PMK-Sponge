16ec815b6db4dce0cdd55911a1d92dcf7f9ee144
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-02 17:55:05 +0100

    Ajout handler pour l'url root

---
+++ src/main/java/fr/mrfern/pumpmysponge/network/http/RootHandler.java
@@ -0,0 +1,22 @@
+package fr.mrfern.pumpmysponge.network.http;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class RootHandler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange he) throws IOException {
+		
+		String response = "<h1>Server start success if you see this message</h1>" + "<h1>Port: " + HttpConfigServer.getPort() + "</h1>";
+        he.sendResponseHeaders(200, response.length());
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+
+	}
+
+}
