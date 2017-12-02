b8773b984576ac3242df67dc496c3618ec31ae94
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-02 17:55:35 +0100

    AJout handler echogethandler

---
+++ src/main/java/fr/mrfern/pumpmysponge/network/http/EchoGetHandler.java
@@ -0,0 +1,16 @@
+package fr.mrfern.pumpmysponge.network.http;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class EchoGetHandler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
