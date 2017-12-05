f49c02fdb9b946a75bcbf3fff4fbc7e6241e9095
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-06 00:04:03 +0100

    Update listener

--- src/main/java/fr/mrfern/pumpmysponge/player/PlayerListenerManager.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/PlayerListenerManager.java
@@ -18,7 +18,8 @@ public class PlayerListenerManager {
 		public void setup() {
 			Sponge.getEventManager().registerListeners(mainManager, new PlayerBanListener(mainManager.getLogger()));
 			Sponge.getEventManager().registerListeners(mainManager, new PlayerConfigListener(mainManager.getLogger()));
-			Sponge.getEventManager().registerListeners(mainManager, new PlayerChatListener(mainManager.getLogger()));
+			Sponge.getEventManager().registerListeners(mainManager, new PlayerChatListener(mainManager));
+			Sponge.getEventManager().registerListeners(mainManager, new PlayerInventoryListener(mainManager));
 		}
 	
 }
