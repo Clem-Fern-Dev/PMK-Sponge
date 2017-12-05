441fc1b5798ff4ca32e290aa76976642597235c9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-06 00:04:11 +0100

    AJout format chat class

--- src/main/java/fr/mrfern/pumpmysponge/player/PlayerChatListener.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/PlayerChatListener.java
@@ -1,23 +1,72 @@
 package fr.mrfern.pumpmysponge.player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.commons.io.filefilter.PrefixFileFilter;
 import org.slf4j.Logger;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.text.Text;
+
+import fr.mrfern.pumpmysponge.Main;
+import me.lucko.luckperms.api.Contexts;
+import me.lucko.luckperms.api.Group;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.caching.MetaData;
+import me.lucko.luckperms.api.caching.UserData;
+import me.lucko.luckperms.api.metastacking.MetaStackDefinition;
+import me.lucko.luckperms.api.metastacking.MetaStackElement;
 
 public class PlayerChatListener {
 
 	@SuppressWarnings("unused")
 	private Logger logger;
+	@SuppressWarnings("unused")
+	private Main main;
 	
-	public PlayerChatListener(Logger logger) {
-		this.logger = logger;
+	public PlayerChatListener(Main main) {
+		this.main = main;
+		this.logger = main.getLogger();
 	}
 	
 	@Listener
-	public void onPlayerSendMessage(MessageChannelEvent.Chat event) {
-		event.setMessage(Text.of("concel message"));
+	public void onPlayerSendMessage(MessageChannelEvent.Chat event, @First Player player) {
+		
+		final LuckPermsApi api = Main.getPermsAPI();
+		String message;
+		
+		Optional<User> user = api.getUserSafe(player.getUniqueId());
+		
+		if(user.isPresent()) {
+			
+			message = "";
+			
+			for (Group group : Main.getGroupList()) {				
+				if(player.hasPermission("group." + group.getName())) {
+					message += " " + group.getCachedData().getMetaData(makeContexts(user.get(), api)).getPrefix();
+				}				
+			}
+			
+			message += user.get().getPrimaryGroup() + "  " + player.getName() + " ";
+			event.setMessage(Text.builder().append(Text.of(message),event.getRawMessage().toBuilder().build()));
+			
+		}	
 	}
+	
+	private Contexts makeContexts(User user,LuckPermsApi api) {
+        Contexts contexts = api.getContextForUser(user).orElse(null);
+        if (contexts == null) {
+            contexts = Contexts.allowAll();
+        }
+        return contexts;
+    }
 	
 
 }
