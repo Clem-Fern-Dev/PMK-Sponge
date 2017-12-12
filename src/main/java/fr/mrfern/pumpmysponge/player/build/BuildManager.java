6fad8e0ce135eac6dec52e879907028be616e891
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-13 00:50:58 +0100

    Ajout du manager de listener dans BuildManager

--- src/main/java/fr/mrfern/pumpmysponge/player/build/BuildManager.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/build/BuildManager.java
@@ -1,5 +1,32 @@
 package fr.mrfern.pumpmysponge.player.build;
 
-public class BuildManager {
+import java.util.HashMap;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
+
+import fr.mrfern.pumpmysponge.Main;
+
+public class BuildManager {
+	private static BuildManager instance = new BuildManager();
+	private static Main mainManager;
+	
+	private HashMap<Player, BuildData> hashBuild = new HashMap<Player, BuildData>();
+
+		public static BuildManager listener(Main main) {
+			mainManager = main;
+			return instance;
+		}
+		 
+		public void setup() {
+			Sponge.getEventManager().registerListeners(mainManager, new BuildListener(mainManager.getLogger()));
+		}
+
+		public HashMap<Player, BuildData> getHashBuild() {
+			return hashBuild;
+		}
+
+		public void setHashBuild(HashMap<Player, BuildData> hashBuild) {
+			this.hashBuild = hashBuild;
+		}
 }
