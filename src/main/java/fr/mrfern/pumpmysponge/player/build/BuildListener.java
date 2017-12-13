5079dc9e499ca2c856ec627857c8569689b2397b
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-13 01:07:58 +0100

    Save, à finir ( logs + return false event + Creative vers Survival )

--- src/main/java/fr/mrfern/pumpmysponge/player/build/BuildListener.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/build/BuildListener.java
@@ -8,6 +8,7 @@ import org.spongepowered.api.event.entity.living.humanoid.ChangeGameModeEvent;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
+import org.w3c.dom.events.EventException;
 
 public class BuildListener {
 	
@@ -20,30 +21,101 @@ public class BuildListener {
 	
 	@Listener
 	public void OnChangeGmSecure(ChangeGameModeEvent.TargetPlayer event,@First Player ply) {
-				
-		if(event.getGameMode().equals(GameModes.CREATIVE) | !event.getOriginalGameMode().equals(GameModes.CREATIVE)) {
+		
+		Player target = event.getTargetEntity();
+		
+		if(event.getGameMode().equals(GameModes.ADVENTURE) & event.getGameMode().equals(GameModes.NOT_SET)) {
+			event.setCancelled(true);			
+		}else {
 			
-			if(ply.hasPermission("group.build") | ply.hasPermission("pumpmysponge.build.safe")) {
+			if(event.getGameMode().equals(GameModes.CREATIVE) | event.getOriginalGameMode().equals(GameModes.SURVIVAL)) {
 				
-				Text textClaimed = Text.builder("Changement de gamemod accordé").color(TextColors.YELLOW).build();
-		    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
-	    		ply.sendMessage(textEnTete);
-	    		
-	    		//ajout du BuildData
-	    		
-			}else {
+				if(target.hasPermission("group.build") | target.hasPermission("pumpmysponge.build.safe")) {
+					
+					if(target.getUniqueId().toString().equals(ply.getUniqueId().toString())) {
+						
+						Text textClaimed = Text.builder("Changement de gamemod accordé").color(TextColors.YELLOW).build();
+				    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+			    		ply.sendMessage(textEnTete);
+			    		
+					}else {
+						
+						Text textClaimed = Text.builder("Changement de gamemod accordé pour le joueur " + target.getName()).color(TextColors.YELLOW).build();
+				    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+			    		ply.sendMessage(textEnTete);
+			    		
+			    		textClaimed = Text.builder("Changement de gamemod accordé, fait par le joueur " + ply.getName()).color(TextColors.YELLOW).build();
+				    	textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+			    		target.sendMessage(textEnTete);
+			    		
+					}
+		    		
+		    		//ajout du BuildData
+		    		
+		    		if(BuildManager.getHashBuild().containsKey(event.getTargetEntity())) {
+		    			
+		    			if(target.getUniqueId().toString().equals(ply.getUniqueId().toString())) {
+							
+							Text textClaimed = Text.builder("Changement de gamemod accordé").color(TextColors.YELLOW).build();
+					    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		ply.sendMessage(textEnTete);
+				    		
+						}else {
+							
+							Text textClaimed = Text.builder("Changement de gamemod accordé pour le joueur " + target.getName()).color(TextColors.YELLOW).build();
+					    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		ply.sendMessage(textEnTete);
+				    		
+				    		textClaimed = Text.builder("Changement de gamemod accordé, fait par le joueur " + ply.getName()).color(TextColors.YELLOW).build();
+					    	textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		target.sendMessage(textEnTete);
+				    		
+						}
+		    			// erreur evenement report
+		    		}else {		    			
+		    			// ajout de la permission de build avec spawn protection
+		    			
+		    			// message
+		    			if(target.getUniqueId().toString().equals(ply.getUniqueId().toString())) {
+							
+							Text textClaimed = Text.builder("Changement de gamemod accordé").color(TextColors.YELLOW).build();
+					    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		ply.sendMessage(textEnTete);
+				    		
+						}else {
+							
+							Text textClaimed = Text.builder("Changement de gamemod accordé pour le joueur " + target.getName()).color(TextColors.YELLOW).build();
+					    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		ply.sendMessage(textEnTete);
+				    		
+				    		textClaimed = Text.builder("Changement de gamemod accordé, fait par le joueur " + ply.getName()).color(TextColors.YELLOW).build();
+					    	textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+				    		target.sendMessage(textEnTete);
+				    		
+						}
+		    			
+		    			// inventaire sauvegardé
+		    			BuildManager.getHashBuild().put(event.getTargetEntity(), new BuildData(event.getTargetEntity()));
+		    		}
+		    		
+				}else {
+					
+					Text textClaimed = Text.builder("Changement de gamemod non accordé").color(TextColors.RED).build();
+			    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
+		    		ply.sendMessage(textEnTete);
+					event.setCancelled(true);
+					
+				}
 				
-				Text textClaimed = Text.builder("Changement de gamemod non accordé").color(TextColors.RED).build();
-		    	Text textEnTete = Text.builder("[ PumpMySafe ] ").color(TextColors.GOLD).append(textClaimed).build();
-	    		ply.sendMessage(textEnTete);
-				event.setCancelled(true);
+			}else if(event.getGameMode().equals(GameModes.SURVIVAL) | event.getOriginalGameMode().equals(GameModes.CREATIVE)) {
+				
+				if(!BuildManager.getHashBuild().containsKey(event.getTargetEntity())) {
+	    			// erreur evenement report
+	    		}else {
+	    			BuildManager.getHashBuild().remove(event.getTargetEntity());
+	    		}
 				
 			}
-			
-		}else if(event.getGameMode().equals(GameModes.SURVIVAL) | event.getOriginalGameMode().equals(GameModes.CREATIVE)) {
-			
-			
-			
 		}
 	}
 	
