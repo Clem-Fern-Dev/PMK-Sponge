31fff6beeceb9eb27eab2f4c0bee9ee907b7ae66
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-13 16:55:35 +0100

    DimensionCommandManager class update

--- src/main/java/fr/mrfern/pumpmysponge/player/world/dimension/DimensionCommandManager.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/world/dimension/DimensionCommandManager.java
@@ -1,5 +1,41 @@
 package fr.mrfern.pumpmysponge.player.world.dimension;
 
-public class DimensionCommandManager {
+import fr.mrfern.pumpmysponge.Main;
 
+public class DimensionCommandManager {
+	private static DimensionCommandManager instance = new DimensionCommandManager();
+	private static Main mainManager;
+
+	public static DimensionCommandManager commands(Main main) {
+		mainManager = main;
+		return instance;
+	}
+	
+	public void setupCommands() {
+		
+		/*CommandSpec commandTempBanSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .permission("pumpmystaff.player.ban")
+			    .executor(new TBanCommand())
+			    .arguments(GenericArguments.flags().flag("all").buildWith(GenericArguments.none()),
+		    				GenericArguments.player(Text.of("player")),
+		    				GenericArguments.integer(Text.of("day")),
+		    				GenericArguments.integer(Text.of("hour")),
+		    				GenericArguments.integer(Text.of("minute")),		    				
+		    				GenericArguments.remainingJoinedStrings(Text.of("raison")))
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandTempBanSpec, "tban");
+		
+		CommandSpec commandUnBanSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .permission("pumpmystaff.player.unban")
+			    .executor(new UnBanCommand())
+			    .arguments(GenericArguments.flags().flag("all").buildWith(GenericArguments.none()),
+	    				   GenericArguments.player(Text.of("player")))
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandUnBanSpec, "unban");*/
+		
+	}
 }
