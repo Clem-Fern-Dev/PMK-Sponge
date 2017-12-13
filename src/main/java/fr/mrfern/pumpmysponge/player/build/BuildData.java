e15247f68ed285d246be1d69caee9268ede62c2e
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-13 01:06:17 +0100

    Update class BuildData

--- src/main/java/fr/mrfern/pumpmysponge/player/build/BuildData.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/build/BuildData.java
@@ -1,5 +1,23 @@
 package fr.mrfern.pumpmysponge.player.build;
 
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.Carrier;
+import org.spongepowered.api.item.inventory.type.CarriedInventory;
+
 public class BuildData {
+
+	private CarriedInventory<? extends Carrier> inventory;
+
+	public BuildData(Player targetEntity) {
+		this.setInventory(targetEntity.getInventory());
+	}
+
+	public CarriedInventory<? extends Carrier> getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(CarriedInventory<? extends Carrier> inventory) {
+		this.inventory = inventory;
+	}
 
 }
