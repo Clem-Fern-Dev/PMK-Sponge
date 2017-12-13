cd218d09df4463625b35d780ba87f17ba36239db
Clem-Fern-Dev <clement.coussieu@gmail.com>
2017-12-13 23:57:06 +0100

    AJout commands spec ( add/delete/create/in queue)

--- src/main/java/fr/mrfern/pumpmysponge/player/world/dimension/DimensionCommandManager.java
+++ src/main/java/fr/mrfern/pumpmysponge/player/world/dimension/DimensionCommandManager.java
@@ -1,6 +1,11 @@
 package fr.mrfern.pumpmysponge.player.world.dimension;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+
 import fr.mrfern.pumpmysponge.Main;
+import fr.mrfern.pumpmysponge.player.world.dimension.commands.CreateDimGroupCommand;
 
 public class DimensionCommandManager {
 	private static DimensionCommandManager instance = new DimensionCommandManager();
@@ -13,20 +18,38 @@ public class DimensionCommandManager {
 	
 	public void setupCommands() {
 		
-		/*CommandSpec commandTempBanSpec = CommandSpec.builder()
+		CommandSpec commandCreateDimGroupSpec = CommandSpec.builder()
 			    .description(Text.of("Commande de bannissement avec durée"))
-			    .permission("pumpmystaff.player.ban")
-			    .executor(new TBanCommand())
-			    .arguments(GenericArguments.flags().flag("all").buildWith(GenericArguments.none()),
-		    				GenericArguments.player(Text.of("player")),
-		    				GenericArguments.integer(Text.of("day")),
-		    				GenericArguments.integer(Text.of("hour")),
-		    				GenericArguments.integer(Text.of("minute")),		    				
-		    				GenericArguments.remainingJoinedStrings(Text.of("raison")))
+			    .permission("pumpmysponge.command.party.create")
+			    .executor(new CreateDimGroupCommand())
 			    .build();
 		
-		Sponge.getCommandManager().register(mainManager, commandTempBanSpec, "tban");
+		Sponge.getCommandManager().register(mainManager, commandCreateDimGroupSpec, "party-c", "party-create");
 		
+		CommandSpec commandDeleteDimGroupSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .permission("pumpmysponge.command.delete")
+			    .executor(new CreateDimGroupCommand())
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandDeleteDimGroupSpec, "party-d", "party-delete");
+		
+		CommandSpec commandAddDimGroupSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .permission("pumpmysponge.command.add")
+			    .executor(new CreateDimGroupCommand())
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandAddDimGroupSpec, "party-a", "party-add");
+		
+		CommandSpec commandQueueDimGroupSpec = CommandSpec.builder()
+			    .description(Text.of("Commande de bannissement avec durée"))
+			    .permission("pumpmysponge.command.inqueu")
+			    .executor(new CreateDimGroupCommand())
+			    .build();
+		
+		Sponge.getCommandManager().register(mainManager, commandQueueDimGroupSpec, "party-q", "party-queue");
+		/*
 		CommandSpec commandUnBanSpec = CommandSpec.builder()
 			    .description(Text.of("Commande de bannissement avec durée"))
 			    .permission("pumpmystaff.player.unban")
